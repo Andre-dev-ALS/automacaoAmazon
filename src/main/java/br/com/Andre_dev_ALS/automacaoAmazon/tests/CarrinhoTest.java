7743dad27c5568b1c2abab44fa09bd759a8dbe0c
Andre_dev_ALS <andreluizsilvasilva2019@gmail.com>
2023-01-20 17:20:29 -0300

    completo o sétimo cenário de teste

--- src/main/java/br/com/Andre_dev_ALS/automacaoAmazon/tests/CarrinhoTest.java
+++ src/main/java/br/com/Andre_dev_ALS/automacaoAmazon/tests/CarrinhoTest.java
@@ -1,23 +1,29 @@
 package br.com.Andre_dev_ALS.automacaoAmazon.tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import br.com.Andre_dev_ALS.automacaoAmazon.core.BaseTest;
 import br.com.Andre_dev_ALS.automacaoAmazon.pages.CarrinhoPage;
 import br.com.Andre_dev_ALS.automacaoAmazon.pages.ProdutoPage;
 
-public class CarrinhoTest extends BaseTest{
-private ProdutoPage produto = new ProdutoPage();
-private CarrinhoPage carrinho = new CarrinhoPage();
+public class CarrinhoTest extends BaseTest {
+	private ProdutoPage produto = new ProdutoPage();
+	private CarrinhoPage carrinho = new CarrinhoPage();
 
-@Test
+	@Test
 
-public void  id007VerificarSeProdutoEstaNoCarrinho() {
-	produto.FazerProcuraDoProduto("frigideira");
-	produto.clicarBotaoProcurar();
-produto.clicarNoProduto("Frigideira");
-carrinho.adicionarProdutoNoCarrinho();
-carrinho.clicarNoCarrinho();
+	public void id007VerificarSeProdutoEstaNoCarrinho() {
+		produto.FazerProcuraDoProduto("frigideira");
+		produto.clicarBotaoProcurar();
+		produto.clicarNoProduto("Frigideira");
+		carrinho.adicionarProdutoNoCarrinho();
+		carrinho.clicarNoCarrinho();
 
-}
+		String nomeDoProdutoNoCarrinho[] = carrinho.confirmarProdutoNoCarrinho().split(" ");
+		String nomeDoProduto = nomeDoProdutoNoCarrinho[0] + ' ' + nomeDoProdutoNoCarrinho[1];
+
+		Assert.assertTrue(carrinho.confirmarProdutoNoCarrinho().startsWith(nomeDoProduto));
+
+	}
 }
