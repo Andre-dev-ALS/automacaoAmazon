a94be0f8da433079f6177d7ca3717fcdc682b8ab
Andre_dev_ALS <andreluizsilvasilva2019@gmail.com>
2023-02-14 00:47:50 -0300

    Completo o décimo terceiro cenário de teste

---
+++ src/main/java/br/com/Andre_dev_ALS/automacaoAmazon/pages/FiltroPage.java
@@ -0,0 +1,27 @@
+package br.com.Andre_dev_ALS.automacaoAmazon.pages;
+
+import static br.com.Andre_dev_ALS.automacaoAmazon.core.DriverFactory.getDriver;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import br.com.Andre_dev_ALS.automacaoAmazon.core.BasePage;
+
+public class FiltroPage extends BasePage {
+	WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(100));
+
+	//public void filtrarMarca() {
+//wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@class = 'a-size-base a-color-base' and contains(.,  'Lenovo')]/../div/label/input")));
+		//clicarCheck(By.xpath("//span[@class = 'a-size-base a-color-base' and contains(.,  'Lenovo')]/../div/label/i"));
+	//}
+	
+public void ordenarPor(String ordenar) {
+	wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"s-result-sort-select\"]")));
+selecionar(By.xpath("//*[@id=\"s-result-sort-select\"]"), ordenar);
+
+
+}
+}
--- src/main/java/br/com/Andre_dev_ALS/automacaoAmazon/tests/FiltroTest.java
+++ src/main/java/br/com/Andre_dev_ALS/automacaoAmazon/tests/FiltroTest.java
@@ -1,13 +1,18 @@
 package br.com.Andre_dev_ALS.automacaoAmazon.tests;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.junit.Test;
 
 import br.com.Andre_dev_ALS.automacaoAmazon.core.BaseTest;
 import br.com.Andre_dev_ALS.automacaoAmazon.pages.MenuPage;
+import br.com.Andre_dev_ALS.automacaoAmazon.pages.ProdutoPage;
 
 public class FiltroTest  extends BaseTest{
 	MenuPage menu = new MenuPage();
-	
+	ProdutoPage produto = new ProdutoPage();
 	
 @Test
 
@@ -16,5 +21,13 @@ public void id013FiltrarProdutosPorCategorias() {
 	menu.clicarBotaoVerMais();
 	menu.clicarFiltrarPor("Computadores e Informática");
 	menu.filtrarSubCategoriaPor("notebooks");
+	
+	List<String> nomeProdutos = produto.obterNomeProdutoEncontrado();
+
+	for (int i = 0; i < nomeProdutos.size(); i++) {
+if(nomeProdutos.get(i).startsWith("Notebook")) {
+	assertTrue(nomeProdutos.get(i).startsWith("Notebook"));
+}
+}
 }
 }
